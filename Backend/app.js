af393297768c9a3ddbbe2e7c3c751d6b57cb6908
Nitya Tripathi <diyatripathi70@gmail.com>
2026-07-02 18:20:32 +0530

    add new files

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+const app = express();
+const port = 8080;
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get("/home", (req, res) => {
+  res.send("this is home route");
+})
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log("listening on port 8080");
+})
---
+++ Backend/src/modals/userModel.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, reuired: true },
+  token: { type: String }
+
+})
+
+const User = mongoose.model("User", userSchema);
+
+export { User };
